94ca34cac46eb0611c28d0e674366c8fe999f1df
BoBoooooo <17746714@qq.com>
2020-04-03 17:30:04 +0800

    perf(code): 新增几处注释

--- src/models/login.js
+++ src/models/login.js
@@ -1,3 +1,9 @@
+/*
+ * @file: 登录model
+ * @copyright: NanJing Anshare Tech .Com
+ * @author: BoBo
+ * @Date: 2020年03月27 15:18:04
+ */
 import { history } from 'umi';
 import { login } from '@/services/login';
 import { setAuthority } from '@/utils/authority';
@@ -8,6 +14,7 @@ const Model = {
     status: undefined,
   },
   effects: {
+    // 登录submit提交后  dispatch到此处发出请求
     *login({ payload }, { call, put }) {
 
       const response = yield call(login, payload);
@@ -16,8 +23,9 @@ const Model = {
       yield put({
         type: 'changeLoginStatus',
         payload: response,
-      }); // Login successfully
-      // 如果登录成功
+      });
+
+      // 如果登录成功,将token存在sessionStorage,网页重定向到首页
       if (code === 200) {
         sessionStorage.setItem('auth',data.token);
         history.push('/');
@@ -34,9 +42,11 @@ const Model = {
     },
   },
   reducers: {
+    // 此处reducers负责修改state中的status值,进行重写
     changeLoginStatus(state, { payload }) {
       setAuthority(['user']);
-      return { ...state, status: payload.status, type: payload.type };
+      // 只复写status这个属性
+      return { ...state, status: payload.status };
     },
   },
 };
