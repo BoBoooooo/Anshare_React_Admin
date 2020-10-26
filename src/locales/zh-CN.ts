3d5ca26963e7061edfae073ce83f039b3112dd4c
BoBoooooo <17746714@qq.com>
2020-10-26 10:15:29 +0800

    perf: remove i18n

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -1,7 +1,5 @@
 import component from './zh-CN/component';
-import globalHeader from './zh-CN/globalHeader';
 import menu from './zh-CN/menu';
-import pwa from './zh-CN/pwa';
 import settingDrawer from './zh-CN/settingDrawer';
 import settings from './zh-CN/settings';
 
@@ -13,10 +11,8 @@ export default {
   'app.preview.down.block': '下载此页面到本地项目',
   'app.welcome.link.fetch-blocks': '获取全部区块',
   'app.welcome.link.block-list': '基于 block 开发，快速构建标准页面',
-  ...globalHeader,
   ...menu,
   ...settingDrawer,
   ...settings,
-  ...pwa,
   ...component,
 };
